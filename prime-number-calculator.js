8a7c56153ed794d4739e172037345d6a4583f0d5
semiCOLON <84295513+semicolon0401@users.noreply.github.com>
2021-06-22 12:53:54 +0900

    Update prime-number-calculator.js

--- prime-number-calculator.js
+++ prime-number-calculator.js
@@ -7,7 +7,19 @@ while (maximum != now_number) {
         }
     }
     if (amount == 2) {
-        document.write(now_number + "<br>");
+            if (now_number < 10) {
+                document.write("0");
+            }
+            if (now_number < 100) {
+                document.write("0")
+            }
+            if (now_number < 1000) {
+                document.write("0")
+            }
+            if (now_number < 10000) {
+                document.write("0")
+            }
+            document.write(now_number + " ");
     }
     number_will_be_divided = 0;
     amount = 0;
